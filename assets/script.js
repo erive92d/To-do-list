1ba3d015af1573f3a92afce05ea4e87609a6fb80
Deorren Erive <erive92d@gmail.com>
2022-11-18 21:13:03 -0800

    testing

--- assets/script.js
+++ assets/script.js
@@ -7,5 +7,35 @@ $('.modal-header').text('Create Todo')
 
 $( '#date' ).datepicker();
 
+// var newTest= document.querySelector('#project-name')
+
+var test = document.createElement('table')
+var table = document.createElement('tr')
+var todo = document.getElementById('todo-name')
+var date = document.getElementById('date')
+
+var tBody = document.querySelector('tbody')
+
+
+
+
+
+
+
+$('#submit').click(function(){
+    var newProj = document.createElement('td')
+    var projDue = document.createElement('td')
+    newProj.classList.add('project-name')
+    projDue.classList.add('project-due')
+    projDue.textContent = date.value
+    newProj.textContent = todo.value;
+    tBody.append(newProj,projDue)
+    
+})
+
+
+
+
+
 
 
